248192da1afc3a97264020593858f28baff86b70
oadam <oadam@efront.com>
2014-12-01 14:09:38 +0100

    No need for useless object creation

--- src/planet.ts
+++ src/planet.ts
@@ -1,33 +1,34 @@
 /// <reference path="vec2.ts"/>
 module Game {
-	export class Planet {
-		constructor(
-			private semiMajorAxis: number,
-			private excentricity: number,
-			private majorAxisLongitude: number,
-			private startOffset: number,
-			private sunAttraction: number) {
-			}
-		getPos(time: number): Vec2.Vec2 {
-			//this function is based on https://github.com/arashaghevli/orbit/blob/master/orbit.js
-			var a = this.semiMajorAxis;
-			var e = this.excentricity;
-			var period = 2 * Math.PI * a* Math.sqrt(a/this.sunAttraction);
-			var M = this.startOffset + 2 * Math.PI * time / period;
+    export interface GetPlanetPositionArg {
+        semiMajorAxis: number;
+        eccentricity: number;
+        majorAxisLongitude: number;
+        startOffset: number;
+    }
 
-			// Compute the eccentric anomaly E from the mean anomaly M and from the eccentricity e (E and M in degrees):
-			var E = M + e * Math.sin(M) * (1.0 + e * Math.cos(M));
-			var error = 1;
-			while (error > 0.005) {
-			var E1 = E - (E - e * Math.sin(E) - M) / (1 - e * Math.cos(E));
-			error = Math.abs(E - E1);
-			E = E1;
-			}
+    //this function is based on https://github.com/arashaghevli/orbit/blob/master/orbit.js
+    export function getPlanetPosition(
+        arg: GetPlanetPositionArg,
+        sunAttraction: number,
+        time: number): Vec2.Vec2 {
+        var a = arg.semiMajorAxis;
+        var e = arg.eccentricity;
+        var period = 2 * Math.PI * a * Math.sqrt(a / sunAttraction);
+        var M = arg.startOffset + 2 * Math.PI * time / period;
 
-			// Then compute the Body's distance r and its true anomaly v from:
-			var x = a * (Math.cos(E) - e);
-			var y = a * (Math.sqrt(1.0 - e*e) * Math.sin(E));
-			return new Vec2.Vec2(x, y).rotate(this.majorAxisLongitude);
-		}
-	}
+        // Compute the eccentric anomaly E from the mean anomaly M and from the eccentricity e (E and M in degrees):
+        var E = M + e * Math.sin(M) * (1.0 + e * Math.cos(M));
+        var error = 1;
+        while (error > 0.005) {
+            var E1 = E - (E - e * Math.sin(E) - M) / (1 - e * Math.cos(E));
+            error = Math.abs(E - E1);
+            E = E1;
+        }
+
+        // Then compute the Body's distance r and its true anomaly v from:
+        var x = a * (Math.cos(E) - e);
+        var y = a * (Math.sqrt(1.0 - e * e) * Math.sin(E));
+        return new Vec2.Vec2(x, y).rotate(arg.majorAxisLongitude);
+    }
 }
--- src/planet_test.ts
+++ src/planet_test.ts
@@ -2,45 +2,46 @@
 /// <reference path="planet.ts" />
 /// <reference path="../bower_components/DefinitelyTyped/jasmine/jasmine.d.ts"/>
 module Game {
-	function vecClose(a: Vec2.Vec2, b: Vec2.Vec2) {
-		expect(a.x).toBeCloseTo(b.x, 4);
-		expect(a.y).toBeCloseTo(b.y, 4);
-	}
-	describe('A circular orbit', function() {
-		var semiMajorAxis = 32;
-		var excentricity = 0;
-		var majorAxisLongitude = 0;
-		var startOffset = 0;
-		var sunAttraction = 50;
-		var period = 2 * Math.PI * semiMajorAxis * Math.sqrt(semiMajorAxis/sunAttraction);
-		var planet = new Planet(semiMajorAxis, excentricity, majorAxisLongitude, startOffset, sunAttraction);
+    function vecClose(a: Vec2.Vec2, b: Vec2.Vec2) {
+        expect(a.x).toBeCloseTo(b.x, 4);
+        expect(a.y).toBeCloseTo(b.y, 4);
+    }
+    describe('A circular orbit', function() {
+        var sunAttraction = 50;
+        var arg = {
+            semiMajorAxis: 32,
+            eccentricity: 0,
+            majorAxisLongitude: 0,
+            startOffset: 0
+        };
+        var period = 2 * Math.PI * arg.semiMajorAxis * Math.sqrt(arg.semiMajorAxis / sunAttraction);
 
-		it('works at time 0', function() {
-			vecClose(planet.getPos(0), new Vec2.Vec2(32, 0));
-		});
-		it('works at period/2', function() {
-			vecClose(planet.getPos(period/2), new Vec2.Vec2(-32, 0));
-		});
-	});
-	describe('A circular orbit', function() {
-		var semiMajorAxis = 50;
-		var excentricity = 0.5;
-		var majorAxisLongitude = 0.30 * Math.PI;
-		var startOffset = 0;
-		var sunAttraction = 10;
-		var period = 2 * Math.PI * semiMajorAxis * Math.sqrt(semiMajorAxis/sunAttraction);
-		var planet = new Planet(semiMajorAxis, excentricity, majorAxisLongitude, startOffset, sunAttraction);
-		it('creates an ellipse of the right length', function() {
-			var start = planet.getPos(0);
-			var end = planet.getPos(period/2);
+        it('works at time 0', function() {
+            vecClose(Game.getPlanetPosition(arg, sunAttraction, 0), new Vec2.Vec2(32, 0));
+        });
+        it('works at period/2', function() {
+            vecClose(Game.getPlanetPosition(arg, sunAttraction, period / 2), new Vec2.Vec2(-32, 0));
+        });
+    });
+    describe('A complex orbit', function() {
+        var sunAttraction = 40;
+        var arg = {
+            semiMajorAxis: 50,
+            eccentricity: 0.5,
+            majorAxisLongitude: 0.30 * Math.PI,
+            startOffset: 0
+        };
+        var period = 2 * Math.PI * arg.semiMajorAxis * Math.sqrt(arg.semiMajorAxis / sunAttraction);
+        it('starts at the given longitude', function() {
+            var start = Game.getPlanetPosition(arg, sunAttraction, 0);
+            expect(Math.atan(start.y / start.x)).toBeCloseTo(arg.majorAxisLongitude, 4);
+        });
+        it('creates an ellipse of the right length', function() {
+            var start = Game.getPlanetPosition(arg, sunAttraction, 0);
+            var end = Game.getPlanetPosition(arg, sunAttraction, period / 2);
 
-			var delta = end.clone().subtract(start);
-			expect(delta.length()).toBeCloseTo(semiMajorAxis*2, 4);
-
-		});
-		it('starts at the given longitude', function() {
-			var start = planet.getPos(0);
-			expect(Math.atan(start.y/start.x)).toBeCloseTo(majorAxisLongitude, 4);
-		});
-	});
+            var delta = end.clone().subtract(start);
+            expect(delta.length()).toBeCloseTo(arg.semiMajorAxis * 2, 4);
+        });
+    });
 }
