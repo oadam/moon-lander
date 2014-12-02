e7ab4670dbc836de9ab86900f02188b074d70b96
oadam <oadam@efront.com>
2014-12-02 10:23:38 +0100

    Added eearth

--- src/controller.ts
+++ src/controller.ts
@@ -7,7 +7,15 @@ module Game {
 		radius: 10 * 1700,
 		semiMajorAxis: 384.0e3,
 		eccentricity: 15 * 0.0549,
-		majorAxisLongitude: 10 / 180 * Math.PI,
+		majorAxisLongitude: 45 / 180 * Math.PI,
+		startOffset: 0
+	};
+	var earth = {
+		id: "earth",
+		radius: 10 * 6400,
+		semiMajorAxis: 1e-5,
+		eccentricity: 1,
+		majorAxisLongitude: 0,
 		startOffset: 0
 	};
 
@@ -24,7 +32,7 @@ module Game {
 		private onFrame(timestamp: number) {
 			if (this.model == null) {
 				//first call
-				this.model = new Game.Model(earthAttraction, [moon], timestamp);
+				this.model = new Game.Model(earthAttraction, [earth, moon], timestamp);
 				this.view = new Game.CanvasView(this.model);
 			}
 			this.model.update(timestamp);
