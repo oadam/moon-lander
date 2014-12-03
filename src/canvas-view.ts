031cac6ecb5456a381eff6b0f5dec70e134e36e5
Olivier Adam <olivieradam666+spam@gmail.com>
2014-12-03 08:38:27 +0100

    Round planets

--- src/canvas-view.ts
+++ src/canvas-view.ts
@@ -23,12 +23,19 @@ module Game {
 			var scale = this.size.length() / modelSize.length();
 			this.ctx.fillStyle = "rgb(200, 0, 0)";
 			this.model.getPlanets().forEach((p) => {
-				var size = p.radius * scale;
+				var size = p.radius * scale * Math.SQRT1_2;
 				var pos = p.position.clone();
 				pos.y *= -1;//canvas coordinates are reversed
 				pos.scale(scale);
 				pos.add(this.size.clone().scale(0.5));
-				this.ctx.fillRect(pos.x - size / 2, pos.y - size / 2, size, size);
+				this.ctx.save();
+				this.ctx.translate(pos.x, pos.y);
+				var SQUARE_COUNT = 6;
+				for (var i = 0; i < SQUARE_COUNT; i++) {
+					this.ctx.rotate(2*Math.PI/(SQUARE_COUNT + 1));
+					this.ctx.fillRect(-size / 2, -size / 2, size, size);
+				}
+				this.ctx.restore();
 			});
 		}
 	}
